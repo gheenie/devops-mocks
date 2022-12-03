c729dc402236e0e28fa76868b9ffacfb7afc7c05
Joe Mulvey <joe.mulvey@northcoders.com>
2022-12-03 20:31:03 +0000

    Change order of tests - again

--- test_cruncher.py
+++ test_cruncher.py
@@ -54,6 +54,19 @@ def test_number_requester_returns_a_valid_result_when_called():
     pass
 
 
+def test_number_requester_returns_error_result_for_non_200_response():
+    """Test that the call method returns a valid item when a request fails.
+    
+    Given:
+         A NumberRequester instance making an unsuccessful call
+
+    Result:
+        A result as a dict in the form {'result': 'FAILURE', 'error_code': 404}
+    
+    """
+    pass
+
+
 def test_number_requester_keeps_log_of_requests():
     """Test that a NumberRequester instance keeps a log of its own requests.
 
@@ -67,19 +80,5 @@ def test_number_requester_keeps_log_of_requests():
         {'request_number': 1, 'call_time': '2022-11-09T16:38:23.417667', 'end_point': 'http://numbersapi.com/random/math',
         'result': 'SUCCESS', 'number': 49}
         
-    """
-    pass
-
-
-
-def test_number_requester_returns_error_result_for_non_200_response():
-    """Test that the call method returns a valid item when a request fails.
-    
-    Given:
-         A NumberRequester instance making an unsuccessful call
-
-    Result:
-        A result as a dict in the form {'result': 'FAILURE', 'error_code': 404}
-    
     """
     pass
